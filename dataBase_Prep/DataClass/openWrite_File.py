51f2f7e1654a272fc9ce5c04bd4b918e091d7027
aoliveir <aoliveir@SAO3-L90007.vnet.valeo.com>
2018-10-04 16:46:22 -0300

    v4

--- dataBase_Prep/DataClass/openWrite_File.py
+++ dataBase_Prep/DataClass/openWrite_File.py
@@ -8,21 +8,11 @@ import csv
 import sys
 import kitti_bbox2D
 import os
-
-'''
-Created on Sep 29, 2018
-
-@author: artamaral
-'''
 from builtins import str
-'''
-Created on 28 de set de 2018
-
-@author: aoliveir
-'''
 
 
 
+#read and write data from training kitti data
 class readTXT():
 
     def readTXT_kittibbox2D(self):
@@ -48,39 +38,39 @@ class readTXT():
         print(path2train)
         print(path2File)
         
-        #img = cv2.imread(path2image,1)
         
+        # Print image
+        #img = cv2.imread(path2image,1)
         #cv2.imshow('image',img)
         #cv2.waitKey(0)
         #cv2.destroyAllWindows()
 
-        
-         
 
-        
         # to separate the file name from path
         head, tail = os.path.split(path2File)
         head2, tail2 = os.path.split(path2image)
 
-        
-       
-        print(path2File.is_file())
-                
+        # while exist a file in the folder, keep doing!        
         while (path2File.is_file()):
                    
-            with open(path2File, mode='r') as csv_file:
+            #Opena file as csv
+            with open(path2File, mode='r',errors="strict") as csv_file:
                 
+                #since files has a space between string...separate  at each space
                 csv_reader = csv.reader(csv_file, delimiter=" ")
                 
+                # for each string in the line
                 for row in csv_reader:
                 
                     #if in the end of file, break!
                     if row[0] == "":
                         break
                     
-                                        #if it's not category I want skip!
+                    #if it's not category I want skip!
                     if row[0] == "Misc" or row[0] == "DontCare":
                         print("Skip: ",row[0])
+                    
+                    #get the fields necessary
                     else:   
                         type = row[0]
                         bbox_l = row[4]
@@ -88,11 +78,13 @@ class readTXT():
                         bbox_r = row[6]
                         bbox_b = row[7]
                         
+                        # do the mats as necessary for darknet
                         x_center = float(bbox_l) + (float(bbox_r) - float(bbox_l))/2
                         y_center = float(bbox_t) + (float(bbox_b) - float(bbox_t))/2
                         x_width = float(bbox_r) - float(bbox_l)
                         y_high = float(bbox_b) - float(bbox_t)        
                         
+                        # if some data  seems to be wrong
                         if x_center < 0 or y_center < 0:
                             print(ArithmeticError)
                             
@@ -110,16 +102,14 @@ class readTXT():
             
             readTXT.writeTXT_train(self,list2train,path2image)
             
+            #get the file name, extract the extension and update the counter... 
             tail = int(tail[:6])
             tail = tail + 1
             
             tail2 = int(tail2[:6])
             tail2 = tail2 + 1
-            
-                   
-            if tail == 5:
-                break
-                        
+        
+            #transform back the updated file name as a string            
             if tail < 10:
                 tail = "00000" + str(tail)
                 tail2 = "00000" + str(tail2)
@@ -140,13 +130,11 @@ class readTXT():
             else:
                 ArithmeticError    
             
+            # Write the full parth of files
             fileName =  str(tail) + ".txt"
-            
             path2File = data_folder / fileName
             path2train = train_folder / fileName
-            #print(path2File)
-           # print(path2train)
-            
+                     
             fileName =  str(tail2) + ".png"
             path2image = image_folder / fileName
             path2image = str(path2image)
